935764249844e0fad8ee85795cff977e25815f17
Pranav H <pranavh002@gmail.com>
2024-09-23 20:46:49 +0530

    Dark mode activation , Toggle Switch in Javascript and Some Styling in CSS

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-const sideMenu = document.querySelector('aside');
-const menuBtn = document.querySelector('#menu_bar');
-const closeBtn = document.querySelector('#close_btn');  // change in line 26 in index.html
+    const sideMenu = document.querySelector('aside');
+    const menuBtn = document.querySelector('#menu_bar');
+    const closeBtn = document.querySelector('#close_btn');  // change in line 26 in index.html
+    const themeToggler = document.querySelector('.theme-toggler')
 
-const themeToggler = document.querySelector('.theme-toggler')
-
-menuBtn.addEventListener('click',()=>{
-    sideMenu.style.display="block"
-})
-menuBtn.addEventListener('click',()=>{
-    sideMenu.style.display="none"
-})
+    menuBtn.addEventListener('click',()=>{
+        sideMenu.style.display="block"
+    })
+    closeBtn.addEventListener('click',()=>{
+        sideMenu.style.display="none"
+    })
 
 
-themeToggler.addEventListener('click',()=>{
+    themeToggler.addEventListener('click',()=>{
 
-    document.body.classList.toggle('dark-theme-variables')
-    
-    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
-    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
+        document.body.classList.toggle('dark-theme-variables')
+        
+        themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+        themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
 
-})
+    })
